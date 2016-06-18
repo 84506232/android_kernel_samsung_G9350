3a5dd82c0406b7a45c112e7d5aab763ec27e320a
msdx321 <msdx321@gmail.com>
2016-06-18 14:27:32 +0800

    Add dtbToolCM

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,7 +8,7 @@ export SEC_BUILD_OPTION_HW_REVISION=02
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android msdx321_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android -j8
 
-dtbToolCM -v -s 2048 -o out/arch/arm64/boot/dt.img  out/arch/arm64/boot/dts/samsung/
+$(pwd)/toolchains/dtbTool/dtbToolCM -v -s 2048 -o out/arch/arm64/boot/dt.img  out/arch/arm64/boot/dts/samsung/
 
 mv out/arch/arm64/boot/dt.img out/dt.img
 mv out/arch/arm64/boot/Image.gz out/kernel.gz
